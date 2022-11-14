68801bd3372b87e005b85f27f12e668936a5ff18
Ankit Kr Dubey <adb020@nist.edu>
2022-11-14 09:56:40 +0530

    Add files via upload

---
+++ practice/main.js
@@ -0,0 +1,14 @@
+let div = document.getElementById('div');
+let box = document.getElementById('box');
+let img = document.getElementById('img');
+
+function lick(){
+  
+  let ankit= document.getElementById("div").innerHTML 
+  document.getElementById("box").innerHTML=ankit 
+
+  let rahul= document.getElementById("pra").innerHTML
+  document.getElementById("box").innerHTML=rahul
+
+}
+console.log('hii')
